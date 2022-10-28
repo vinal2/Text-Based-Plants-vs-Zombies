2969520644706a0e8644ab2a3940b6bf15e0a501
vinal2 <prestons2523@gmail.com>
2022-10-28 16:52:43 -0700

    added json handling and made tests (making more tests)

---
+++ src/test/model/readTest.java
@@ -0,0 +1,43 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+import persistence.JsonReader;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class readTest {
+    private static final String filePath = "./data/gameData.json";
+    Game game = new Game();
+    JsonReader reader = new JsonReader(filePath);
+
+    @Test
+    void readEmptyFileTest() {
+        reader = new JsonReader("./data/wysi.json");
+        try {
+            reader.readJson(game);
+            fail("IOException expected");
+        } catch (IOException e) {
+
+        }
+    }
+
+    @Test
+    void readEmptyGameTest() {
+        reader = new JsonReader("./data/emptyGame.json");
+        try {
+            reader.readJson(game);
+            assertEquals(game.getZombieList().size(), 0);
+            assertEquals(game.getPlantList().size(), 0);
+        } catch (IOException e) {
+            fail("file doesn't exist");
+        }
+    }
+
+    @Test
+    void readGameTest() {
+
+    }
+}
